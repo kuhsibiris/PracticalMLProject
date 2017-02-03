c1f58563a1abc7f847f61c6f068cbd334d2031ef
Andres Mejia <kuhsibiris@gmail.com>
2017-02-02 23:00:59 -0500

    final

--- Analysis.R
+++ Analysis.R
@@ -3,6 +3,8 @@ library(caret)
 library(purrr)
 library(tidyr)
 library(doParallel)
+library(corrplot)
+library(Matrix)
 
 
 rm(list=ls())
@@ -40,18 +42,31 @@ trainingpre=preProcess(training,c("center","scale","knnImpute"))
 training2<-predict(trainingpre,training)
 training2$X<-NULL
 
-#the following variables [1] "roll_belt"        "total_accel_belt" "max_roll_belt"    "max_picth_belt"   "min_roll_belt"   
-#[6] "min_pitch_belt"   "avg_roll_belt"    "avg_yaw_belt"     "accel_belt_y"
-# are highly correlated with one another will remove all but avg_roll_belt that seems to be the most correlated
-# with the rest of variables, others are detected as full colineality
 
-removecols<-c("min_yaw_belt","amplitude_pitch_arm" ,
-        "min_roll_dumbbell","max_yaw_forearm","amplitude_yaw_arm","min_yaw_dumbbell")
+# #the following variables [1] "roll_belt"        "total_accel_belt" "max_roll_belt"    "max_picth_belt"   "min_roll_belt"   
+# #[6] "min_pitch_belt"   "avg_roll_belt"    "avg_yaw_belt"     "accel_belt_y"
+# # are highly correlated with one another will remove all but avg_roll_belt that seems to be the most correlated
+# # with the rest of variables, others are detected as full colineality
+# 
+# removecols<-c("min_yaw_belt","amplitude_pitch_arm" ,
+#         "min_roll_dumbbell","max_yaw_forearm","amplitude_yaw_arm","min_yaw_dumbbell")
+# 
+# training2<-training2 %>% dplyr::select(-one_of(removecols))
 
-training2<-training2 %>% dplyr::select(-one_of(removecols))
+rankfist<-function(i){
+rankMatrix(training2[colnames(training2)%in% colsnum][1:i])
+}
+
+fin<-(training2[colnames(training2)%in% colsnum] %>% dim)[2]
+milista<-map(1:fin,rankfist)
+milista2<-map(milista,`[[`,1)
+unlist(milista2)
+colnames(training2[colnames(training2)%in% colsnum])[c(10,66,95)]
+to_remove2<-colnames(training2[colnames(training2)%in% colsnum])[c(10,66,95)]
+training2<-training2 %>% dplyr::select(-one_of(to_remove2))
 
 train_control<-trainControl(method = "cv",number = 10)
-
+training2$user_name<-training2$raw_timestamp_part_1<-training2$raw_timestamp_part_2<-training2$cvtd_timestamp
 modelo1<-train(data=training2,method="lda",classe~.-1,trControl=train_control)
 
 data1<-predict(trainingpre,testing)
